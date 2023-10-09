1900e3e46be3cf585c69f8a7011c11e17bbb63d2
Naman Rajput <namanrajput801@gmail.com>
2023-10-09 17:47:27 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+$("#header-carousel").owlCarousel({
+  center: true,
+  // autoplay: true,
+  stagePadding: 20,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  items: 1,
+  responsive: {
+    768: {
+      items: 3,
+    },
+  },
+});
+
+$("#product-carousel-1").owlCarousel({
+  margin: 15,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  loop: true,
+  items: 1,
+  responsive: {
+    768: {
+      items: 3,
+    },
+    1024: {
+      items: 4,
+    },
+  },
+});
+$("#product-carousel-2").owlCarousel({
+  margin: 15,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  loop: true,
+  items: 1,
+  responsive: {
+    768: {
+      items: 3,
+    },
+    1024: {
+      items: 4,
+    },
+  },
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
