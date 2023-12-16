40c593f51181f42e03cc6607ca0035dcdc94032e
Naman Rajput <namanrajput801@gmail.com>
2023-12-16 16:34:06 +0530

    ui:add some panorama examples

---
+++ borabora/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-beach-1",
+      "name": "beach (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 913.5,
+      "initialViewParameters": {
+        "yaw": -0.3980467299502024,
+        "pitch": 0.4409808447558845,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
